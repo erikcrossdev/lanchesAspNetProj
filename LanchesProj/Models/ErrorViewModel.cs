a12e3e14e93c48145bcc2c578ea7bd6ff2b104c4
ErikCruz <erikcruzdev@gmail.com>
2026-08-31 00:42:39 -0300

    Class 11, project creation

---
+++ LanchesProj/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LanchesProj.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ LanchesProj/Program.cs
@@ -0,0 +1,17 @@
+namespace LanchesProj;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args)
+           .Build()
+           .Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
