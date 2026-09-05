a5f54de598ea85261b065233521a48b750baf63f
ErikCruz <erikcruzdev@gmail.com>
2026-09-04 22:25:14 -0300

    Create demo view, add a carousel partial view, add images. Classes 13, 14, 15 and 16

--- LanchesProj/Controllers/HomeController.cs
+++ LanchesProj/Controllers/HomeController.cs
@@ -12,6 +12,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Demo()
+    {
+        return View();
+    }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
