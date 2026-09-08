45a370856a80c664e85ba42286523a29e3bc73d4
ErikCruz <erikcruzdev@gmail.com>
2026-09-07 21:38:49 -0300

    Class 17. Creating category and meal class

---
+++ LanchesProj/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace LanchesProj.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; } //tendo o nome Id ou CategoriaId, o EFC vai setar como primary key
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+        //Propriedade de navegação para ideintificar a relação entre as entidades Categoria e Lanche, ou seja, uma categoria pode ter vários lanches
+        public List<Lanche> Lanches { get; set; } //propriedade de navegação para os lanches
+    }
+}
---
+++ LanchesProj/Models/Lanche.cs
@@ -0,0 +1,18 @@
+﻿namespace LanchesProj.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; } //tendo o nome Id ou CategoriaId, o EFC vai setar como primary key
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+        //Para definir o relacionamento usando a navegação e a chave estrangeira
+        public int CategoriaId { get; set; } //chave estrangeira
+        public virtual Categoria Categoria { get; set; } //propriedade de navegação para a categoria
+    }
+}
