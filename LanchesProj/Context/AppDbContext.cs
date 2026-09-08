659a2f748fdbce2f44faec29b8e8fa166751fd1c
ErikCruz <erikcruzdev@gmail.com>
2026-09-07 22:38:06 -0300

    Class 18-19. Add DbContext Class, connection strings, and add EFCore.

---
+++ LanchesProj/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using LanchesProj.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesProj.Context
+{
+    public class AppDbContext : DbContext
+    {
+        //Carrega informações de opções de configurações 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {
+
+        }
+
+        //Define classes mapeadas para tabelas no banco de dados
+        public DbSet<Lanche> Lanches { get; set; } //representa a tabela Lanches no banco de dados
+        public DbSet<Categoria> Categorias { get; set; } //representa a tabela Categorias no banco de dados
+    }
+}
