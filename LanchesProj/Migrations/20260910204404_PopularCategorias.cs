0d8a5455e9536e098f9c91cc90be5e7d765d90e2
ErikCruz <erikcruzdev@gmail.com>
2026-09-10 17:52:22 -0300

    27. Adding default values in to Categorias Database

---
+++ LanchesProj/Migrations/20260910204404_PopularCategorias.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesProj.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias (CategoriaNome, Descricao) "+
+                "VALUES ('Normal', 'Lanche feito com carne')");
+
+            migrationBuilder.Sql("INSERT INTO Categorias (CategoriaNome, Descricao) " +
+           "VALUES ('Vegetariano', 'Lanche feito sem produtos de origem animal')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias"); //remove tudo da tabela Categorias
+        }
+    }
+}
