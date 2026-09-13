50afad2dbcf17290756025c6ce43dfa2306dbd64
ErikCruz <erikcruzdev@gmail.com>
2026-09-13 00:12:46 -0300

    Create Lanche Controller and Lanche List view

---
+++ LanchesProj/Controllers/LancheController.cs
@@ -0,0 +1,22 @@
+﻿using LanchesProj.Repositories;
+using LanchesProj.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesProj.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository lancheRepository; //usa a instancia
+
+        public LancheController(ILancheRepository lancheRepository) //usar a dependecy injection
+        {
+            this.lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
