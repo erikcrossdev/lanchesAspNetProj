ea1999594da35ce47d59c7a5ffe43dc3dd01ba77
ErikCruz <erikcruzdev@gmail.com>
2026-09-23 13:57:10 -0300

    57. Create Tag helper

---
+++ LanchesProj/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace LanchesProj.TagHelpers
+{
+	public class EmailTagHelper : TagHelper
+	{
+		public string Endereco { get; set; }
+		public string Conteudo { get; set; }
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "a";
+			output.Attributes.SetAttribute("href", $"mailto:{Endereco}");
+            output.Attributes.SetAttribute("target", "_blank"); // Adicionado para garantir o disparo do protocolo mailto
+            output.Content.SetContent(Conteudo);
+		}
+	}
+}
