bb8f35cbc70827fa281c45f876fea901d12069f7
ErikCruz <erikcruzdev@gmail.com>
2026-09-24 14:16:42 -0300

    61. Display a dropdown with all categories

---
+++ LanchesProj/Components/CategoriaMenu.cs
@@ -0,0 +1,21 @@
+﻿using LanchesProj.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesProj.Components
+{
+	public class CategoriaMenu : ViewComponent
+	{
+		private readonly ICategoriaRepository _categoriaRepository;
+
+		public CategoriaMenu(ICategoriaRepository categoriaRepository)
+		{
+			_categoriaRepository = categoriaRepository;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			var categorias = _categoriaRepository.Categorias.OrderBy(c => c.CategoriaNome); //ordena por nome
+			return View(categorias);
+		}
+	}
+}
