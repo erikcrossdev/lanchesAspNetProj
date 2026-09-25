c2e5094263987cecbffdd2ebbe8eac8f7165ab71
ErikCruz <erikcruzdev@gmail.com>
2026-09-25 10:05:17 -0300

    62. Showing categories in a data driven way

--- LanchesProj/Controllers/LancheController.cs
+++ LanchesProj/Controllers/LancheController.cs
@@ -27,17 +27,10 @@ namespace LanchesProj.Controllers
 			}
 			else
 			{
-                //vamos mudar isso para pegar qualquer categoria
-                if (string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase)){
-                    lanches = _lancheRepository.Lanches
-                        .Where(l => l.Categoria.CategoriaNome.Equals("Normal"))
-                        .OrderBy(l => l.Nome);
-                }
-                else {
-					lanches = _lancheRepository.Lanches
-						.Where(l => l.Categoria.CategoriaNome.Equals("Vegetariano"))
-						.OrderBy(l => l.Nome);
-				}
+               lanches = _lancheRepository.Lanches
+                    .Where(l => l.Categoria.CategoriaNome==categoria)
+                    .OrderBy(l => l.Nome);
+                categoriaAtual = categoria; 
 			}
 			
             var lanchesListViewModel = new LancheListViewModel
